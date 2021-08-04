825ff66de2ffa67233fc9ab02b8890e45f08c0c3
dor-co <dor713cohen@gmail.com>
2021-08-04 11:42:07 +0300

    favorite list

--- src/screens/favorite/Favorite.js
+++ src/screens/favorite/Favorite.js
@@ -1,10 +1,48 @@
 import '../../App.css';
+import "firebase/firestore";
+import { useFirestore } from "reactfire";
+import { useState, useEffect } from 'react';
 
 function Favorite() {
+  const [firebaseData, setFirebaseData] = useState([]);
+
+  const db = useFirestore();
+
+  const useItems = (itemType, callback, items) => {
+    useEffect(() => {
+      const fetchData = async () => {
+        await db
+          .collection(itemType)
+          .onSnapshot((snapshot) => {
+            let listItems = [];
+
+            listItems = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            callback(listItems);
+          });
+      };
+      fetchData();
+    }, []);
+    return items;
+  };
+
+  useItems("Weathers", setFirebaseData, firebaseData);
+
+  console.log(firebaseData)
+
   return (
-      <div>
-        <h1>favorite</h1>
-      </div>
+    <div>
+      <h1>favorite list</h1>
+      {firebaseData
+        .filter(e => e.isFavorite === true)
+        .map(item => {
+          return(
+            <h3>{item.testField}</h3>
+          );
+        })}
+    </div>
   );
 }
 
